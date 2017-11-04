74c483b72a58abe3d83a0a244949bf8131ff5ffb
Daniel Huong <daniel.huong86@gmail.com>
2017-11-04 20:01:20 +1100

    completed SellerProfile form and added validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,5 @@
 class Profile < ApplicationRecord
   validates_presence_of :first_name, :last_name, :phone_number, :street_address, :suburb, :state
-  validates :phone_number, format: { with: /\A\d{10}\z/,
-  message: "numbers only" }
+  validates :phone_number, format: { with: /\A\d{10}\z/, message: "numbers only" }
   belongs_to :user 
 end
--- app/models/seller_profile.rb
+++ app/models/seller_profile.rb
@@ -2,6 +2,8 @@ class SellerProfile < ApplicationRecord
   include ShopPhotoUploader[:shop_photo]
   include UserPhotoUploader[:user_photo]
   belongs_to :user
+  validates_presence_of :shop_name, :cuisine_type
+  validates :delivery_cost, numericality: { greater_than_or_equal_to: 0, less_than: 20 }
 
-  CUISINE_TYPES = ["Chinese", "French", "Italian", "Japanese", "Mexican", "Spanish", "Ethiopian", "Thai", "Africaaaaan", "Vietnamese"]
+  CUISINE_TYPES = ["Continental", "Chinese", "French", "Italian", "Japanese", "Mexican", "Spanish", "Ethiopian", "Thai", "African", "Vietnamese"]
 end
