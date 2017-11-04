1bb77209a55f90484045c82b20ff761bbb993a06
Daniel Huong <daniel.huong86@gmail.com>
2017-11-04 19:28:36 +1100

    added validation of Profile phone number to be numbers only

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be reject phone numbers that don't contain digits" do
+    profile = Profile.new(first_name: 'Pickle', last_name: 'Rick', street_address: '36 Lakewood Bvd', suburb:'Cairnlea', state:'VIC', phone_number: 'abc')
+    profile1 = Profile.new(first_name: 'Pickle', last_name: 'Rick', street_address: '36 Lakewood Bvd', suburb:'Cairnlea', state:'VIC', phone_number: '0421119005')
+    expect(profile1).to be_valid
+    expect(profile).to be_invalid
+  end
 end
