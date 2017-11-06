d471b00d3c62bfbc8708f798bedcc668bc8f2387
Daniel Huong <daniel.huong86@gmail.com>
2017-11-06 12:11:13 +1100

    Completed Messages CSS

--- config/routes.rb
+++ config/routes.rb
@@ -5,28 +5,32 @@ Rails.application.routes.draw do
   # Landing Page
   root 'landing#index', as: 'home'
 
-  get '/dashboard/', to: 'dashboard#index', as: 'dashboard' do 
+  get '/dashboard/', to: 'dashboard#index', as: 'dashboard' 
+  
   # Routes for profile
-    get '/myprofile', to: 'profiles#show', as: 'profile'
-    post '/profiles', to: 'profiles#create', as: 'profiles'
-    get '/myprofile/new', to: 'profiles#new', as: 'new_profile'
-    get '/myprofile/edit', to: 'profiles#edit', as: 'edit_profile'
-    patch '/myprofile', to: 'profiles#update'
-    put '/myprofile', to: 'profiles#update'
-    # resources :profiles, only: [:new, :edit, :create, :update]
-    
-    # Routes for seller profile
-    get '/sellerprofile', to: 'seller_profiles#show', as: 'seller_profile'
-    post '/sellerprofiles', to: 'seller_profiles#create', as: 'seller_profiles'
-    get '/sellerprofile/new', to: 'seller_profiles#new', as: 'new_seller_profile'
-    get '/sellerprofile/edit', to: 'seller_profiles#edit', as: 'edit_seller_profile'
-    patch '/sellerprofile', to: 'seller_profiles#update'
-    put '/sellerprofile', to: 'seller_profiles#update'
-  end
+  get '/myprofile', to: 'profiles#show', as: 'profile'
+  post '/profiles', to: 'profiles#create', as: 'profiles'
+  get '/myprofile/new', to: 'profiles#new', as: 'new_profile'
+  get '/myprofile/edit', to: 'profiles#edit', as: 'edit_profile'
+  patch '/myprofile', to: 'profiles#update'
+  put '/myprofile', to: 'profiles#update'
+  # resources :profiles, only: [:new, :edit, :create, :update]
+  
+  # Routes for seller profile
+  get '/sellerprofile', to: 'seller_profiles#show', as: 'seller_profile'
+  post '/sellerprofiles', to: 'seller_profiles#create', as: 'seller_profiles'
+  get '/sellerprofile/new', to: 'seller_profiles#new', as: 'new_seller_profile'
+  get '/sellerprofile/edit', to: 'seller_profiles#edit', as: 'edit_seller_profile'
+  patch '/sellerprofile', to: 'seller_profiles#update'
+  put '/sellerprofile', to: 'seller_profiles#update'
+
   
   
   # Routes for items
   resources :items
 
-
+  # Routes for conversations
+  resources :conversations do
+    resources :messages
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104091615) do
+ActiveRecord::Schema.define(version: 20171105115245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.bigint "seller_profile_id"
@@ -26,6 +33,30 @@ ActiveRecord::Schema.define(version: 20171104091615) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["seller_profile_id"], name: "index_items_on_seller_profile_id"
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.date "date"
+    t.time "open"
+    t.time "close"
+    t.bigint "item_id"
+    t.integer "quantity"
+    t.bigint "seller_profile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_listings_on_item_id"
+    t.index ["seller_profile_id"], name: "index_listings_on_seller_profile_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.boolean "read", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -76,6 +107,10 @@ ActiveRecord::Schema.define(version: 20171104091615) do
   end
 
   add_foreign_key "items", "seller_profiles"
+  add_foreign_key "listings", "items"
+  add_foreign_key "listings", "seller_profiles"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "seller_profiles", "users"
 end
