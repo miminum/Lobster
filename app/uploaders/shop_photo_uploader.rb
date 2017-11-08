3d57c77fd22abcd91e50de7f03cafa22b42a6b8c
Daniel Huong <daniel.huong86@gmail.com>
2017-11-08 23:54:25 +1100

    Favicon added

--- app/uploaders/shop_photo_uploader.rb
+++ app/uploaders/shop_photo_uploader.rb
@@ -7,7 +7,7 @@ class ShopPhotoUploader < Shrine
     plugin :remove_attachment
     plugin :store_dimensions
     plugin :validation_helpers
-    plugin :versions, names: [:original, :store, :thumb]
+    plugin :versions, names: [:original, :large, :medium, :thumb]
   
     Attacher.validate do
       validate_max_size 2.megabytes, message: 'is too large (max is 2 MB)'
@@ -17,10 +17,11 @@ class ShopPhotoUploader < Shrine
     def process(io, context)
       case context[:phase]
       when :store
-        size_400 = resize_to_limit!(io.download, 400, 300)
+        size_700 = resize_to_limit!(io.download, 700, 530)
+        size_400 = resize_to_limit(size_700, 200, 200)
         thumb = resize_to_limit(size_400, 200, 200)
         
-        { original: io, medium: size_400, thumb: thumb }
+        { original: io, large: size_700, medium: size_400, thumb: thumb }
       end
     end
   end
